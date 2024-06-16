fe35161333ba6a70f75c244cfc8bd3e5246b6b6a
yereeem <132246494+yereeem@users.noreply.github.com>
2024-06-17 11:27:02 +1200

    - Initialised class numVowels.java - parsed the input word from the user (in the command line) - temporary pseudocode for searching for the number of vowels in that word.

---
+++ numVowels.java
@@ -0,0 +1,26 @@
+class numVowels{
+    public static void main(String[] args){
+        // Check that there is exactly 1 command line argument (the input word for analysis).
+        if (args.length != 1){
+            System.out.println("Usage: java numVowels <\"word\">");
+            return;
+        }
+
+        // Get the word from the command line argument.
+        String word = args[0];
+
+        // METHOD countVowels(word)
+
+            // look through the word character by character.
+
+            // set up a counter variable (set to 0).
+
+            // increment the counter for every vowel found.
+
+            // once the end of the word has been reached, 
+            // stop searching
+            // print the number of vowels found.
+
+        //EXIT method
+    }
+}
