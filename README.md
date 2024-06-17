a32e77ca0c4a3811f1fb9c27b83b668d4c66be95
yereeem <132246494+yereeem@users.noreply.github.com>
2024-06-17 12:02:30 +1200

    implemented the countVowels method to: - initialise a counter to keep track of the number of vowels in the word - look through the word character by character and compare it with a set list of vowels - increment the counter if there is a match - print the number of vowels present in the word to console.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # count-vowels
+
+## This class houses a function to search through any given word via the command line, and print back how many vowels are present in that word.
+
+## Example usage: java numVowels "caterpillar"
--- numVowels.java
+++ numVowels.java
@@ -9,18 +9,35 @@ class numVowels{
         // Get the word from the command line argument.
         String word = args[0];
 
-        // METHOD countVowels(word)
+        // get the number of vowels in the given word.
+        int numVowels = countVowels(word);
 
-            // look through the word character by character.
+        // print the number of vowels present the in the given word.
+        System.out.println("There are " + numVowels + " vowels in \""+ word + "\"");
+    }
 
-            // set up a counter variable (set to 0).
+    private static int countVowels(String word){
+        // set up a counter variable initially set to 0.
+        int count = 0;
 
-            // increment the counter for every vowel found.
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
 
-            // once the end of the word has been reached, 
-            // stop searching
-            // print the number of vowels found.
+        // set up a variable to look at the current character.
+        char c;
 
-        //EXIT method
+        // look through the word character by character.
+        for (int i = 0; i <= word.length() - 1; i++){
+            c = word.charAt(i);
+            
+            // look through the list of vowels.
+            for (int j = 0; j <= vowels.length - 1; j++){
+                // if the current character in the word is a vowel, increment the vowel count.
+                if (c == vowels[j]){
+                    count++; 
+                }
+            }
+        }
+
+        return count;
     }
 }
